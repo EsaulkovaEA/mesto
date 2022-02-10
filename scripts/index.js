d76a8906df71d7feb4b39fc91fc0cad7ca224ee5
EsaulkovaEA <esaulkovaea@gmail.com>
2022-02-10 22:17:54 +0500

    исправление после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,35 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__edit-button");
+let formElement = document.querySelector(".popup__content");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
 
-// const togglePopupVisibility = function(){
-//     popupElement.classList.toggle('popup_opened');
-// };
 const openPopup = function () {
   popupElement.classList.add("popup_opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 };
 const closePopup = function () {
   popupElement.classList.remove("popup_opened");
 };
-const closePopupByClickOnOverlay = function (event) {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  closePopup();
-};
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopupByClickOnOverlay);
-
-let formElement = document.querySelector(".popup__content");
-let nameInput = document.querySelector(".popup__input_type_name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector(".popup__input_type_job"); // Воспользуйтесь инструментом .querySelector()
+// const closePopupByClickOnOverlay = function (event) {
+//   if (event.target !== event.currentTarget) {
+//     return;
+//   }
+//   closePopup();
+// };
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  document.querySelector(".profile__title").textContent = nameInput.value;
-  document.querySelector(".profile__subtitle").textContent = jobInput.value;
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
   closePopup();
 }
 
-document.querySelector(".profile__title").textContent = nameInput.value;
-document.querySelector(".profile__subtitle").textContent = jobInput.value;
-
 formElement.addEventListener("submit", formSubmitHandler);
+popupOpenButtonElement.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+// popupElement.addEventListener("click", closePopupByClickOnOverlay);
