ca59ecd121b6dfa2c52537d4a95400ac53a5d353
EsaulkovaEA <esaulkovaea@gmail.com>
2022-02-14 22:01:59 +0500

    popup-add-place

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 const popupElement = document.querySelector(".popup");
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-let formElement = document.querySelector(".popup__content");
-let nameInput = document.querySelector(".popup__input_type_name");
-let jobInput = document.querySelector(".popup__input_type_job");
-let profileTitle = document.querySelector(".profile__title");
-let profileSubtitle = document.querySelector(".profile__subtitle");
+const popupOpenAddButtonElement = document.querySelector(".profile__add-button");
+const formElement = document.querySelector(".popup__content");
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_job");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+
 
 const openPopup = function () {
   popupElement.classList.add("popup_opened");
