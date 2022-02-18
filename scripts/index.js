a661c2a29b6a5f45da3cba920463fa7fd97e08e1
EsaulkovaEA <esaulkovaea@gmail.com>
2022-02-18 15:09:30 +0500

    add template

--- scripts/index.js
+++ scripts/index.js
@@ -28,22 +28,30 @@ const initialCards = [
 const popupProfileElement = document.querySelector(".popup_edit-profile");
 const popupCloseProfileElement = popupProfileElement.querySelector(".popup__close");
 const popupOpenProfileButtonElement = document.querySelector(".profile__edit-button");
-const formElement = document.querySelector(".popup__content");
-const nameInput = document.querySelector(".popup__input_type_name");
-const jobInput = document.querySelector(".popup__input_type_job");
+const formElement = popupProfileElement.querySelector(".popup__content");
+const nameInput = popupProfileElement.querySelector(".popup__input_type_name");
+const jobInput = popupProfileElement.querySelector(".popup__input_type_job");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
 const popupPlaceElement = document.querySelector(".popup_add-place");
 const popupClosePlaceElement = popupPlaceElement.querySelector(".popup__close");
+const formPlaceElement = popupPlaceElement.querySelector(".popup__content");
 const popupOpenPlaceButtonElement = document.querySelector(".profile__add-button");
-// const elementItems = document.querySelector(".element__items");
-// const cardsTemplate = document.querySelector(".#cards-template").content;
+
+
+const elementItems = document.querySelector(".element__items");
+const cardsTemplate = document.querySelector("#cards-template").content;
+// клонируем содержимое тега template
+const cardsElement = cardsTemplate.querySelector('.element').cloneNode(true);
+
+
 
 
 // в функцию const cardsElement = cardsTemplate.querySelector('.element').cloneNode(true);
 const popupImageElement = document.querySelector(".popup_view-image");
 const popupCloseImageElement = popupImageElement.querySelector(".popup__close");
+
 const popupOpenImageButtonElement = document.querySelector(".element");
 
 
@@ -62,14 +70,24 @@ function closePopup (popup) {
 //   closePopup();
 // };
 
-function formSubmitHandler(evt) {
+
+
+formElement.addEventListener("submit", function formSubmitEditProfile(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
   closePopup(popupProfileElement);
-}
+});
 
-formElement.addEventListener("submit", formSubmitHandler);
+formPlaceElement.addEventListener("submit", function formSubmitAddPlace(evt) {
+  evt.preventDefault();
+
+  closePopup(popupPlaceElement);
+});
+
+
+
+
 popupOpenProfileButtonElement.addEventListener("click", function()
 {
   openPopup(popupProfileElement);
